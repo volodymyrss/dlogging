efa6c39fdb896a45bea8c153637a90ab12fbb92b
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-02 20:30:38 +0200

    requires

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
                 "*.py"
                 ]
             },
-         install_requires=[
-            'zmq',
+        install_requires=[
+        'zmq',
         ],
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
         long_description=open('README.md').read(),
